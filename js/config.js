6d463e97918b8320d71c290bf4d38384f865b360
LathikaGS <135726112+LathikaGS@users.noreply.github.com>
2024-11-09 10:36:36 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,9 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_xhkKQnUJH', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7cjqnf6aea6ah0mcgg4preb3mb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'us-east-1_xhkKQnUJH', 
+        userPoolClientId: '7cjqnf6aea6ah0mcgg4preb3mb', 
+        region: 'us-east-1' 
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+        invokeUrl: 'https://resje7xbqb.execute-api.us-east-1.amazonaws.com/dev' 
 };
